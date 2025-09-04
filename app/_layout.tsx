a3ed9759c3691ca91d2a022849f952ad0f632e7a
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-09-04 11:36:43 +0900

    🌱 기본 설정값 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,13 @@ export default function RootLayout() {
   return (
     <>
       <StatusBar style='auto' />
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack
+        screenOptions={{
+          headerShown: false, // 기본 헤더 숨기기
+          navigationBarHidden: true, // // OS 바텀내비게이션바 숨기기
+          statusBarHidden: true, // OS 상태바는 숨기기
+        }}
+      >
         <Stack.Screen name='index' />
         <Stack.Screen name='news' />
       </Stack>
